e31634794802d289a509a106f9d0a0d4fbdd0982
Anzelika Starceva <anzelikastar0806@gmail.com>
2024-05-22 15:11:57 +0300

    added categories to inventory

---
+++ Assets/Script/DisplayText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+    public Text nameText;
+
+    void Start()
+    {
+        string playerName = PlayerPrefs.GetString("PlayerName", "Без имени");
+        nameText.text = "Привет, " + playerName;
+    }
+}
--- Assets/Script/DoNotDestroy.cs
+++ Assets/Script/DoNotDestroy.cs
@@ -6,12 +6,21 @@ public class DoNotDestroy : MonoBehaviour
 {
     private void Awake()
     {
+        // Check if there are multiple instances of the GameObject with the "GameMusic" tag
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
-        if(musicObj.Length > 1)
+        if (musicObj.Length > 1)
         {
             Destroy(this.gameObject);
+            return;
         }
 
+        // Ensure the GameObject is at the root level
+        if (this.transform.parent != null)
+        {
+            this.transform.SetParent(null);
+        }
+
+        // Prevent the GameObject from being destroyed on scene load
         DontDestroyOnLoad(this.gameObject);
     }
 }
